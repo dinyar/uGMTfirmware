f110df191e96caa0a1f3aa7761ff63226f00f7d0
Dinyar Rabady <dinyar@rabady.at>
2014-10-30 21:03:08 +0100

    Generated RAM cores for relative and absolute isolation. Both memories have also been filled with coe files. This fixes #3, and begins both #6 and #19.

---
+++ uGMT_algos/firmware/hdl/cores/rel_isolation_mem/simulation/functional/wave_ncsim.sv
@@ -0,0 +1,26 @@
+
+
+ 
+ 
+ 
+
+ 
+
+
+
+window new WaveWindow  -name  "Waves for BMG Example Design"
+waveform  using  "Waves for BMG Example Design"
+
+      waveform add -signals /rel_isolation_mem_tb/status
+      waveform add -signals /rel_isolation_mem_tb/rel_isolation_mem_synth_inst/bmg_port/CLKA
+      waveform add -signals /rel_isolation_mem_tb/rel_isolation_mem_synth_inst/bmg_port/ADDRA
+      waveform add -signals /rel_isolation_mem_tb/rel_isolation_mem_synth_inst/bmg_port/DINA
+      waveform add -signals /rel_isolation_mem_tb/rel_isolation_mem_synth_inst/bmg_port/WEA
+      waveform add -signals /rel_isolation_mem_tb/rel_isolation_mem_synth_inst/bmg_port/DOUTA
+      waveform add -signals /rel_isolation_mem_tb/rel_isolation_mem_synth_inst/bmg_port/CLKB
+      waveform add -signals /rel_isolation_mem_tb/rel_isolation_mem_synth_inst/bmg_port/ADDRB
+      waveform add -signals /rel_isolation_mem_tb/rel_isolation_mem_synth_inst/bmg_port/DINB
+      waveform add -signals /rel_isolation_mem_tb/rel_isolation_mem_synth_inst/bmg_port/WEB
+      waveform add -signals /rel_isolation_mem_tb/rel_isolation_mem_synth_inst/bmg_port/DOUTB
+
+console submit -using simulator -wait no "run"
