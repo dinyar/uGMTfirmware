ce1af4ed6342c0e7c3c9570862a6930b657fcf35
Dinyar Rabady <dinyar@rabady.at>
2014-11-25 18:46:35 +0100

    Added new patterns to update scripts. Resolves #85.

--- uGMT_algos/firmware/testbench/serializer/update_testfiles.sh
+++ uGMT_algos/firmware/testbench/serializer/update_testfiles.sh
@@ -8,3 +8,15 @@ wget https://github.com/jlingema/uGMTScripts/raw/master/ugmt_patterns/data/patte
 mv serializer_sort_test.txt.1 serializer_sort_test.txt
 wget https://github.com/jlingema/uGMTScripts/raw/master/ugmt_patterns/data/patterns/testbench/serializer_many_events.txt --no-check-certificate
 mv serializer_many_events.txt.1 serializer_many_events.txt
+wget https://github.com/jlingema/uGMTScripts/raw/master/ugmt_patterns/data/patterns/testbench/serializer_SingleMuPt100.txt --no-check-certificate
+mv serializer_SingleMuPt100.txt.1 serializer_SingleMuPt100.txt
+wget https://github.com/jlingema/uGMTScripts/raw/master/ugmt_patterns/data/patterns/testbench/serializer_ZMM.txt --no-check-certificate
+mv serializer_ZMM.txt.1 serializer_ZMM.txt
+wget https://github.com/jlingema/uGMTScripts/raw/master/ugmt_patterns/data/patterns/testbench/serializer_WM.txt --no-check-certificate
+mv serializer_WM.txt.1 serializer_WM.txt
+wget https://github.com/jlingema/uGMTScripts/raw/master/ugmt_patterns/data/patterns/testbench/serializer_FakeMuons.txt --no-check-certificate
+mv serializer_FakeMuons.txt.1 serializer_FakeMuons.txt
+wget https://github.com/jlingema/uGMTScripts/raw/master/ugmt_patterns/data/patterns/testbench/serializer_MinBias.txt --no-check-certificate
+mv serializer_MinBias.txt.1 serializer_MinBias.txt
+wget https://github.com/jlingema/uGMTScripts/raw/master/ugmt_patterns/data/patterns/testbench/serializer_TTbar.txt --no-check-certificate
+mv serializer_TTbar.txt.1 serializer_TTbar.txt
--- uGMT_algos/firmware/testbench/sort_and_cancel/update_testfiles.sh
+++ uGMT_algos/firmware/testbench/sort_and_cancel/update_testfiles.sh
@@ -8,3 +8,13 @@ wget https://github.com/jlingema/uGMTScripts/raw/master/ugmt_patterns/data/patte
 mv sort_test.txt.1 sort_test.txt
 wget https://github.com/jlingema/uGMTScripts/raw/master/ugmt_patterns/data/patterns/testbench/many_events.txt --no-check-certificate
 mv many_events.txt.1 many_events.txt
+wget https://github.com/jlingema/uGMTScripts/raw/master/ugmt_patterns/data/patterns/testbench/SingleMuPt100.txt --no-check-certificate
+mv SingleMuPt100.txt.1 SingleMuPt100.txt
+wget https://github.com/jlingema/uGMTScripts/raw/master/ugmt_patterns/data/patterns/testbench/WM.txt --no-check-certificate
+mv WM.txt.1 WM.txt
+wget https://github.com/jlingema/uGMTScripts/raw/master/ugmt_patterns/data/patterns/testbench/TTbar.txt --no-check-certificate
+mv TTbar.txt.1 TTbar.txt
+wget https://github.com/jlingema/uGMTScripts/raw/master/ugmt_patterns/data/patterns/testbench/ZMM.txt --no-check-certificate
+mv ZMM.txt.1 ZMM.txt
+wget https://github.com/jlingema/uGMTScripts/raw/master/ugmt_patterns/data/patterns/testbench/FakeMuons.txt --no-check-certificate
+mv FakeMuons.txt.1 FakeMuons.txt
