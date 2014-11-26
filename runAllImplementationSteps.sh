da7f8c0f746bb7a198ebe62b64b944408ec0a34b
Dinyar Rabady <dinyar@rabady.at>
2014-11-26 11:24:31 +0100

    Build script now fails if synthesis fails. Closes #87.

--- runAllImplementationSteps.sh
+++ runAllImplementationSteps.sh
@@ -12,8 +12,8 @@ warning && exit 1
 
 # Check for Xilinx environment
 if [ -z "$XILINX" ]; then
-echo "No Xilinx, no party"
-return
+    echo "No Xilinx, no party"
+    exit 1
 fi
 
 ## CUSTOMIZE THESE VARIABLES
@@ -24,6 +24,10 @@ NO_RUNS=14 # Number of smartxplorer runs
 ## END OF CUSTOMIZATION
 
 xtclsh uGMT.tcl
-smartxplorer -p xc7vx690t-2-ffg1927 -l $HOSTS_FILE -m $NO_RUNS -uc "../cactusupgrades/boards/mp7/base_fw/common/firmware/ucf/area_constraints.ucf;../cactusupgrades/boards/mp7/base_fw/common/firmware/ucf/clock_constraints.ucf;../cactusupgrades/boards/mp7/base_fw/common/firmware/ucf/mp7_pins.ucf;../cactusupgrades/boards/mp7/base_fw/mp7_690es/firmware/ucf/mp7.ucf;../cactusupgrades/components/uGMT_algos/firmware/ucf/algo_constraints.ucf" -wd $WORKING_DIR -rcmd ssh top.ngd
-## Uncomment the following (and comment the above smartxplorer call) if the custom strategy file should be used)
-#smartxplorer -p xc7vx690t-2-ffg1927 -l $HOSTS_FILE -m $NO_RUNS -sf $STRATEGIES_FILE -uc "../cactusupgrades/boards/mp7/base_fw/common/firmware/ucf/area_constraints.ucf;../cactusupgrades/boards/mp7/base_fw/common/firmware/ucf/clock_constraints.ucf;../cactusupgrades/boards/mp7/base_fw/common/firmware/ucf/mp7_pins.ucf;../cactusupgrades/boards/mp7/base_fw/mp7_690es/firmware/ucf/mp7.ucf;../cactusupgrades/components/uGMT_algos/firmware/ucf/algo_constraints.ucf" -wd $WORKING_DIR -rcmd ssh top.ngd
+
+if [ $? -eq 0 ]
+then
+    smartxplorer -p xc7vx690t-2-ffg1927 -l $HOSTS_FILE -m $NO_RUNS -uc "../cactusupgrades/boards/mp7/base_fw/common/firmware/ucf/area_constraints.ucf;../cactusupgrades/boards/mp7/base_fw/common/firmware/ucf/clock_constraints.ucf;../cactusupgrades/boards/mp7/base_fw/common/firmware/ucf/mp7_pins.ucf;../cactusupgrades/boards/mp7/base_fw/mp7_690es/firmware/ucf/mp7.ucf;../cactusupgrades/components/uGMT_algos/firmware/ucf/algo_constraints.ucf" -wd $WORKING_DIR -rcmd ssh top.ngd
+    ## Uncomment the following (and comment the above smartxplorer call) if the custom strategy file should be used)
+    # smartxplorer -p xc7vx690t-2-ffg1927 -l $HOSTS_FILE -m $NO_RUNS -sf $STRATEGIES_FILE -uc "../cactusupgrades/boards/mp7/base_fw/common/firmware/ucf/area_constraints.ucf;../cactusupgrades/boards/mp7/base_fw/common/firmware/ucf/clock_constraints.ucf;../cactusupgrades/boards/mp7/base_fw/common/firmware/ucf/mp7_pins.ucf;../cactusupgrades/boards/mp7/base_fw/mp7_690es/firmware/ucf/mp7.ucf;../cactusupgrades/components/uGMT_algos/firmware/ucf/algo_constraints.ucf" -wd $WORKING_DIR -rcmd ssh top.ngd
+fi
