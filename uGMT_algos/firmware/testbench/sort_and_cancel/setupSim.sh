3dcfaf1b530614636ca526cd106a66f8710ebe7b
Dinyar Rabady <dinyar@rabady.at>
2015-03-11 13:47:47 +0100

    Work on simulation for #110

--- uGMT_algos/firmware/testbench/sort_and_cancel/setupSim.sh
+++ uGMT_algos/firmware/testbench/sort_and_cancel/setupSim.sh
@@ -10,19 +10,23 @@ PATTERNFILE=ugmt_testfile.dat
 
 vlib sortAndCancel_tb
 vmap work sortAndCancel_tb
+vcom ../../cgn/mem_libs/blk/blk_mem_gen_v8_2.vhd
+vcom -check_synthesis ../../cgn/mem_libs/dist/dist_mem_gen_v8_0.vhd
+vcom -check_synthesis ../../cgn/mem_libs/dist/dist_mem_gen_v8_0_vh_rfs.vhd
+vcom -check_synthesis ../../cgn/mem_libs/dist/dist_mem_gen_v8_0_vhsyn_rfs.vhd
 vcom -check_synthesis ../../hdl/common/ugmt_constants.vhd
 vcom -check_synthesis $CACTUSREPOPATH/cactusupgrades/components/mp7_datapath/firmware/hdl/mp7_data_types.vhd
 vcom -check_synthesis ../../hdl/common/GMTTypes_pkg.vhd
 vcom -check_synthesis $CACTUSREPOPATH/cactusupgrades/components/ipbus_core/firmware/hdl/ipbus_package.vhd
 vcom -check_synthesis ../../hdl/ipbus_decode_sorting.vhd 
 vcom -check_synthesis /home/scratch/vhdl/uGMT/vivado/prod/1.5.2/cactusupgrades/components/ipbus_core/firmware/hdl/ipbus_fabric_sel.vhd 
+vcom -check_synthesis ../../hdl/Sorting/SorterUnit.vhd
 vcom -check_synthesis ../../hdl/MatchAndMerge/*
 vcom -check_synthesis ../../hdl/GhostBusting/GhostCheckerUnit.vhd
 vcom -check_synthesis ../../cgn/cancel_out_mem.vhd
 vcom -check_synthesis ../../hdl/GhostBusting/GhostCheckerUnit_spatialCoords.vhd 
 vcom -check_synthesis ../../hdl/GhostBusting/WedgeCheckerUnit.vhd 
 vcom -check_synthesis ../../hdl/GhostBusting/CancelOutUnit_*
-vcom -check_synthesis ../../hdl/Sorting/SorterUnit.vhd
 vcom -check_synthesis ../../hdl/Sorting/Stage0/SortStage0_countWins.vhd
 vcom -check_synthesis ../../hdl/Sorting/Stage0/SortStage0_Mux.vhd 
 vcom -check_synthesis ../../hdl/Sorting/Stage0/SortStage0_behavioral.vhd 
