92ad13c4fb05a4f52d27ffa066eb8c73dceb8d6e
Dinyar Rabady <dinyar.rabady@cern.ch>
2015-03-12 21:29:49 +0100

    Fixed sort and cancel testbench. This closes #110.

--- uGMT_algos/firmware/testbench/sort_and_cancel/setupSim.sh
+++ uGMT_algos/firmware/testbench/sort_and_cancel/setupSim.sh
@@ -11,28 +11,31 @@ PATTERNFILE=ugmt_testfile.dat
 vlib sortAndCancel_tb
 vmap work sortAndCancel_tb
 vcom ../../cgn/mem_libs/blk/blk_mem_gen_v8_2.vhd
-vcom -check_synthesis ../../cgn/mem_libs/dist/dist_mem_gen_v8_0.vhd
-vcom -check_synthesis ../../cgn/mem_libs/dist/dist_mem_gen_v8_0_vh_rfs.vhd
-vcom -check_synthesis ../../cgn/mem_libs/dist/dist_mem_gen_v8_0_vhsyn_rfs.vhd
 vcom -check_synthesis ../../hdl/common/ugmt_constants.vhd
 vcom -check_synthesis $CACTUSREPOPATH/cactusupgrades/components/mp7_datapath/firmware/hdl/mp7_data_types.vhd
 vcom -check_synthesis ../../hdl/common/GMTTypes_pkg.vhd
 vcom -check_synthesis $CACTUSREPOPATH/cactusupgrades/components/ipbus_core/firmware/hdl/ipbus_package.vhd
-vcom -check_synthesis ../../hdl/ipbus_decode_sorting.vhd 
-vcom -check_synthesis /home/scratch/vhdl/uGMT/vivado/prod/1.5.2/cactusupgrades/components/ipbus_core/firmware/hdl/ipbus_fabric_sel.vhd 
+vcom -check_synthesis ../../hdl/ipbus_decode_sorting.vhd
+vcom -check_synthesis $CACTUSREPOPATH/cactusupgrades/components/ipbus_core/firmware/hdl/ipbus_fabric_sel.vhd
 vcom -check_synthesis ../../hdl/Sorting/SorterUnit.vhd
 vcom -check_synthesis ../../hdl/MatchAndMerge/*
 vcom -check_synthesis ../../hdl/GhostBusting/GhostCheckerUnit.vhd
-vcom -check_synthesis ../../cgn/cancel_out_mem.vhd
-vcom -check_synthesis ../../hdl/GhostBusting/GhostCheckerUnit_spatialCoords.vhd 
-vcom -check_synthesis ../../hdl/GhostBusting/WedgeCheckerUnit.vhd 
+vcom -check_synthesis $CACTUSREPOPATH/cactusupgrades/components/ipbus_slaves/firmware/hdl/ipbus_dpram.vhd
+vcom -check_synthesis ../../hdl/ipbus_decode_cancel_out_*.vhd
+vcom -check_synthesis ../../hdl/GhostBusting/GhostCheckerUnit_spatialCoords.vhd
+vcom -check_synthesis ../../hdl/GhostBusting/WedgeCheckerUnit.vhd
+vcom -check_synthesis ../../hdl/GhostBusting/CancelOutUnit_BO_WedgeComp.vhd
+vcom -check_synthesis ../../hdl/GhostBusting/CancelOutUnit_BO.vhd
+vcom -check_synthesis ../../hdl/GhostBusting/CancelOutUnit_FO_WedgeComp.vhd
+vcom -check_synthesis ../../hdl/GhostBusting/CancelOutUnit_FO.vhd
 vcom -check_synthesis ../../hdl/GhostBusting/CancelOutUnit_*
+vcom -check_synthesis ../../hdl/common/comp10_ge_behavioral.vhd
 vcom -check_synthesis ../../hdl/Sorting/Stage0/SortStage0_countWins.vhd
-vcom -check_synthesis ../../hdl/Sorting/Stage0/SortStage0_Mux.vhd 
-vcom -check_synthesis ../../hdl/Sorting/Stage0/SortStage0_behavioral.vhd 
-vcom -check_synthesis ../../hdl/Sorting/Stage0/HalfSortStage0.vhd 
-vcom -check_synthesis ../../hdl/Sorting/Stage1/SortStage1_behavioral.vhd 
-vcom -check_synthesis ../../hdl/Sorting/SortAndCancelUnit.vhd 
+vcom -check_synthesis ../../hdl/Sorting/Stage0/SortStage0_Mux.vhd
+vcom -check_synthesis ../../hdl/Sorting/Stage0/SortStage0_behavioral.vhd
+vcom -check_synthesis ../../hdl/Sorting/Stage0/HalfSortStage0.vhd
+vcom -check_synthesis ../../hdl/Sorting/Stage1/SortStage1_behavioral.vhd
+vcom -check_synthesis ../../hdl/Sorting/SortAndCancelUnit.vhd
 vcom -check_synthesis ../tb_helpers.vhd
 vcom -check_synthesis SortAndCancelUnit_tb.vhd
 vmake work > Makefile
@@ -43,4 +46,3 @@ then
     rm -f $PATTERNFILE
 fi
 ln -s many_events.txt $PATTERNFILE
-
