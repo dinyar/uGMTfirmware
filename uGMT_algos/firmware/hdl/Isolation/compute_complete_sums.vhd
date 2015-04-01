320a47a62bfc9d4deb9e1b00eeb421f50f2c24fe
Dinyar Rabady <dinyar@rabady.at>
2015-04-01 22:16:54 +0200

    Fixes #145.

--- uGMT_algos/firmware/hdl/Isolation/compute_complete_sums.vhd
+++ uGMT_algos/firmware/hdl/Isolation/compute_complete_sums.vhd
@@ -4,6 +4,7 @@ use IEEE.STD_LOGIC_1164.all;
 use IEEE.NUMERIC_STD.all;
 
 use work.GMTTypes.all;
+use work.ugmt_constants.all;
 
 entity compute_complete_sums is
   port (iEnergies     : in  TCaloRegionEtaSlice_vector;
@@ -12,6 +13,8 @@ entity compute_complete_sums is
         iCaloIdxBitsF  : in  TCaloIndexBit_vector(35 downto 0);
         iMuIdxBits     : in  TIndexBits_vector(7 downto 0);
         oEnergies      : out TCaloArea_vector(7 downto 0);
+        oCaloIdxBits   : out TCaloIndexBit_vector(7 downto 0); -- Debugging output
+
         clk            : in  std_logic;
         sinit          : in  std_logic);
 end compute_complete_sums;
@@ -32,7 +35,8 @@ begin
   reg_calo_bits : process (clk, sinit)
   begin  -- process reg_calo_bits
     if clk'event and clk = '0' then     -- falling clock edge
-      sMergedCaloIdxBits <= iCaloIdxBitsB & iCaloIdxBitsO & iCaloIdxBitsF;
+      sMergedCaloIdxBits <= iCaloIdxBitsF(35 downto 18) & iCaloIdxBitsO(35 downto 18) &
+        iCaloIdxBitsB & iCaloIdxBitsO(17 downto 0) & iCaloIdxBitsF(17 downto 0);
       sMuIdxBits_reg     <= iMuIdxBits;
       sEnergies_reg      <= iEnergies;
     end if;
@@ -47,12 +51,14 @@ begin
       sCaloIdxBits(i) <= sMergedCaloIdxBits(to_integer(sMuIdxBits_reg(i)));
     end loop;  -- i
   end process assign_index_bits;
-  
+
   extract_strip_energies : process (sCaloIdxBits, sEnergies_reg)
   begin  -- process extract_strip_energies
     for i in oEnergies'range loop
-      oEnergies(i) <= sEnergies_reg(to_integer(sCaloIdxBits(i).eta))((to_integer(sCaloIdxBits(i).phi)+2) mod sEnergies_reg(0)'length);
+      oEnergies(i) <= sEnergies_reg(to_integer(sCaloIdxBits(i).eta))((to_integer(sCaloIdxBits(i).phi)) mod sEnergies_reg(0)'length);
     end loop;  -- i
   end process extract_strip_energies;
-  
+
+  oCaloIdxBits <= sCaloIdxBits;
+
 end Behavioral;
