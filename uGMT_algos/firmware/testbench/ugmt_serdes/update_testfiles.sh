2c038d3e7d071b7baf5175c2750aeb0848abf9de
Dinyar Rabady <dinyar@rabady.at>
2015-04-16 00:37:18 +0200

    Added new testfiles for #167.

--- uGMT_algos/firmware/testbench/ugmt_serdes/update_testfiles.sh
+++ uGMT_algos/firmware/testbench/ugmt_serdes/update_testfiles.sh
@@ -5,3 +5,8 @@ if [ -f integration_many_events.txt.1 ];
 then
 	mv integration_many_events.txt.1 integration_many_events.txt
 fi
+wget https://github.com/jlingema/uGMTScripts/raw/master/ugmt_patterns/data/patterns/testbench/integration_iso_test.txt --no-check-certificate
+if [ -f integration_iso_test.txt.1 ];
+then
+	mv integration_iso_test.txt.1 integration_iso_test.txt
+fi
