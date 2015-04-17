1711b81db674fef9a205f9635488f94c6a3be51c
Dinyar Rabady <dinyar@rabady.at>
2015-04-17 18:05:45 +0200

    Modified build scripts for #164.

--- uGMT_algos/firmware/testbench/runAll.sh
+++ uGMT_algos/firmware/testbench/runAll.sh
@@ -10,17 +10,17 @@ then
 	mkdir results
 fi
 cd deserializer
-./runSim.sh > /dev/null
+./runSim.sh &> /dev/null
 cd ../GMT
-./runSim.sh > /dev/null
+./runSim.sh &> /dev/null
 cd ../isolation
-./runSim.sh > /dev/null
+./runSim.sh &> /dev/null
 cd ../serializer
-# ./runSim.sh > /dev/null
+./runSim.sh &> /dev/null
 cd ../sort_and_cancel
-./runSim.sh > /dev/null
+./runSim.sh &> /dev/null
 cd ../ugmt_serdes
-./runSim.sh > /dev/null
+./runSim.sh &> /dev/null
 cd ..
 
 python ../../../scripts/check_results.py
--- uGMT_algos/firmware/testbench/setupAndRunAll.sh
+++ uGMT_algos/firmware/testbench/setupAndRunAll.sh
@@ -5,34 +5,53 @@ export MGLS_LICENSE_FILE='1717@lxlic01:1717@lxlic02:1717@lxlic03'
 PATH=$PATH:/opt/mentor/modeltech/linux_x86_64
 export PATH
 
+if [ $# -gt 0 ];
+then
+	testfile=$1
+else
+	testfile=many_events
+fi
+
 if [ ! -d results ];
 then
 	mkdir results
 fi
 echo "Running deserializer testbench.. "
 cd deserializer
-./setupSim.sh > /dev/null
-./runSim.sh > /dev/null
+./setupSim.sh &> /dev/null
+rm -f ugmt_testfile.dat
+ln -s deserializer_$testfile.txt ugmt_testfile.dat
+./runSim.sh &> /dev/null
 echo "Running GMT testbench.. "
 cd ../GMT
-./setupSim.sh > /dev/null
-./runSim.sh > /dev/null
+./setupSim.sh &> /dev/null
+rm -f ugmt_testfile.dat
+ln -s $testfile.txt ugmt_testfile.dat
+./runSim.sh &> /dev/null
 echo "Running Isolation testbench.. "
 cd ../isolation
-./setupSim.sh > /dev/null
-./runSim.sh > /dev/null
+./setupSim.sh &> /dev/null
+rm -f ugmt_testfile.dat
+ln -s $testfile.txt ugmt_testfile.dat
+./runSim.sh &> /dev/null
 echo "Running Serializer testbench.. "
 cd ../serializer
-./setupSim.sh > /dev/null
-# ./runSim.sh > /dev/null
+./setupSim.sh &> /dev/null
+rm -f ugmt_testfile.dat
+ln -s serializer_$testfile.txt ugmt_testfile.dat
+#./runSim.sh &> /dev/null
 echo "Running SortAndCancel testbench.. "
 cd ../sort_and_cancel
-./setupSim.sh > /dev/null
-./runSim.sh > /dev/null
+./setupSim.sh &> /dev/null
+rm -f ugmt_testfile.dat
+ln -s $testfile.txt ugmt_testfile.dat
+./runSim.sh &> /dev/null
 echo "Running uGMTserdes testbench.. "
 cd ../ugmt_serdes
-./setupSim.sh > /dev/null
-./runSim.sh > /dev/null
+./setupSim.sh &> /dev/null
+rm -f ugmt_testfile.dat
+ln -s integration_$testfile.txt ugmt_testfile.dat
+./runSim.sh &> /dev/null
 cd ..
 
 echo "Checking results.. "
