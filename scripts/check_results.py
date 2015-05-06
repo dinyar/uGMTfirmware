cf25041632b4ab88b844a2d6551dd5adc91ba9b6
Joschka Lingemann <joschka.lingemann@cern.ch>
2015-05-06 20:54:42 +0200

    Now checking if all testbench results are present. Fixes #206.

--- scripts/check_results.py
+++ scripts/check_results.py
@@ -3,33 +3,28 @@ import os
 import sys
 
 
-def encode_error(testbench_name):
-    if testbench_name == "GMT":
-        return 1
-    if testbench_name == "serializer":
-        return 2
-    if testbench_name == "deserializer":
-        return 4
-    if testbench_name == "SortAndCancel":
-        return 8
-    if testbench_name == "ugmt_serdes":
-        return 16
-    if testbench_name == "isolation":
-        return 32
+def encode_error(testbench_name, names):
+    error_code = 1 << names.index(testbench_name)
+    return error_code
 
 
 def main():
     error = 0
+    testbench_names = ["GMT", "serializer", "deserializer", "SortAndCancel", "ugmt_serdes", "isolation"]
+    file_counter = 0
     for root, folders, fnames in os.walk('results'):
         for fname in fnames:
             if fname.endswith(".results"):
                 with open(os.path.join(root, fname), 'r') as fobj:
+                    file_counter += 1
                     lines = fobj.readlines()
                     res = lines[-1]
                     n_err = int(res.split(":")[1].strip())
                     if n_err != 0:
                         tbname = fname.split("_tb")[0]
-                        error += encode_error(tbname)
+                        error += encode_error(tbname, testbench_names)
+    if file_counter != len(testbench_names):
+        return -1
     return error
 
 
