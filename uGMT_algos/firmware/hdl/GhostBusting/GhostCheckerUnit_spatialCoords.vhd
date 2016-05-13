83506decae2593c856bef2474eaa089b5ec41c52
Dinyar Rabady <dinyar@rabady.at>
2016-05-13 12:17:08 +0200

    Adapted dEta check to larger cancel-out LUTs. This belongs to #347.

--- uGMT_algos/firmware/hdl/GhostBusting/GhostCheckerUnit_spatialCoords.vhd
+++ uGMT_algos/firmware/hdl/GhostBusting/GhostCheckerUnit_spatialCoords.vhd
@@ -83,7 +83,7 @@ begin
   check_ghosts : process (match, qual1, qual2, deltaPhi, deltaEta)
   begin  -- process check_ghosts
     -- If the muons are 'far enough' apart we don't check the LUT output.
-    if (deltaPhi(7 downto 3) /= (4 downto 0 => '0')) or (deltaEta(8 downto 4) /= (4 downto 0 => '0')) then
+    if (deltaPhi(7 downto 3) /= (4 downto 0 => '0')) or (deltaEta(8 downto 5) /= (3 downto 0 => '0')) then
       ghost1 <= '0';
       ghost2 <= '0';
     elsif match = "1" then
