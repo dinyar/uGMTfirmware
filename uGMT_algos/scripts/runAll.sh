22e88890dbd0b686df782cbbce0f4f25e545953d
Dinyar Rabady <dinyar.rabady@cern.ch>
2016-05-20 13:56:09 +0200

    Make build script work with Jenkins. Needed for #349.

--- uGMT_algos/scripts/runAll.sh
+++ uGMT_algos/scripts/runAll.sh
@@ -9,33 +9,21 @@ source /home/scratch/Vivado2016.1/Vivado/2016.1/settings64.sh
 PROJECTFILE="top/top.xpr"
 
 ##### Create files #####
-cat << EOM > setupSynthesis.tcl
-open_project $PROJECTFILE
-launch_runs synth_1 -scripts_only
-exit
-EOM
-
-cat << EOM > setupImplementation.tcl
-open_project $PROJECTFILE
-set_property STEPS.PHYS_OPT_DESIGN.IS_ENABLED true [get_runs impl_1]
-launch_runs impl_1 -scripts_only
-exit
-EOM
-
 cat << EOM > makeBitfile.tcl
-open_project $PROJECTFILE
-launch_runs impl_1 -to_step write_bitstream -scripts_only
+open_project $(PROJECTFILE)
+launch_runs synth_1
+wait_on_run synth_1
+set_property STEPS.PHYS_OPT_DESIGN.IS_ENABLED true [get_runs impl_1]
+launch_runs impl_1
+wait_on_run impl_1
+launch_runs impl_1 -to_step write_bitstream
+wait_on_run impl_1
 exit
 EOM
 
 ## Reset project and run steps up to bitfile generation and packaging.
 make reset
-vivado -mode batch -source setupSynthesis.tcl
-bash -ex top/top.runs/synth_1/runme.sh
-vivado -mode batch -source setupImplementation.tcl
-bash -ex top/top.runs/impl_1/runme.sh
 vivado -mode batch -source makeBitfile.tcl
-bash -ex top/top.runs/impl_1/runme.sh
 
 python checkTiming.py
 
