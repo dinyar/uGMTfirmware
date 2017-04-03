0251302cccb4d4e400331d169b12cedd5a315355
Dinyar Rabady <dinyar.rabady@cern.ch>
2017-04-03 09:05:49 +0200

    Small fixes to #381.

--- uGMT_algos/firmware/testbench/setupAll.sh
+++ uGMT_algos/firmware/testbench/setupAll.sh
@@ -3,10 +3,18 @@ export MODELSIM_ROOT='/opt/modelsim-104d/modeltech/'
 ## license server
 export MGLS_LICENSE_FILE='1717@lxlic01:1717@lxlic02:1717@lxlic03'
 PATH=$PATH:/opt/modelsim-104d/modeltech/linux_x86_64
-export PATH
+export $PATH
+
+mp7fw_repo_subdir=tags/mp7/stable/firmware/mp7fw_v2_2_1
+if [ ! $# == 1 ];
+then
+    echo "mp7fw release not given. Setting to "$mp7fw_repo_subdir"."
+else
+    mp7fw_repo_subdir=$1
+fi
 
 echo "Updating test pattern and LUT content files.. "
-bash update_testfiles.sh
+bash update_testfiles.sh $mp7fw_repo_subdir
 
 echo "Building serializer testbench.. "
 cd serializer
@@ -21,4 +29,3 @@ cd ../ugmt_serdes
 ./buildSim.sh
 
 cd ..
-
--- uGMT_algos/firmware/testbench/update_testfiles.sh
+++ uGMT_algos/firmware/testbench/update_testfiles.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+if [ $# -lt 1 ];
+then
+    echo "Error, expecting mp7fw release path."
+    exit
+fi
+mp7fw_repo_subdir=$1
+
 if [ ! -d patterns ];
 then
-	mkdir patterns
+    mkdir patterns
 fi
 
 # Get the test patterns
@@ -10,3 +17,31 @@ python ../../scripts/get_testpatterns.py testbench --outpath patterns/.
 
 # Update the LUT content files.
 python ../../scripts/get_luts.py binary --outpath ../hdl/ipbus_slaves/.
+
+# Get mp7fw
+if [ ! -d "cactusupgrades" ]; then
+    mkdir cactusupgrades
+    pushd cactusupgrades
+    mkdir -p boards/mp7
+    pushd boards/mp7
+    wget -O base_fw.zip https://svnweb.cern.ch/trac/cactus/browser/$MP7FW_REPO_SUBDIR/cactusupgrades/boards/mp7/base_fw?rev=HEAD\&format=zip
+    unzip base_fw.zip
+    rm -f base_fw.zip
+    popd
+    mkdir components
+    pushd components
+    wget -O ipbus_slaves.zip https://svnweb.cern.ch/trac/cactus/browser/$MP7FW_REPO_SUBDIR/cactusupgrades/components/ipbus_slaves?rev=HEAD\&format=zip
+    unzip ipbus_slaves.zip
+    rm -f ipbus_slaves.zip
+    wget -O ipbus_core.zip https://svnweb.cern.ch/trac/cactus/browser/$MP7FW_REPO_SUBDIR/cactusupgrades/components/ipbus_core?rev=HEAD\&format=zip
+    unzip ipbus_core.zip
+    rm -f ipbus_core.zup
+    wget -O mp7_datapath.zip https://svnweb.cern.ch/trac/cactus/browser/$MP7FW_REPO_SUBDIR/cactusupgrades/components/mp7_datapath?rev=HEAD\&format=zip
+    unzip mp7_datapath.zip
+    rm -f mp7_datapath.zip
+    wget -O mp7_ttc.zip https://svnweb.cern.ch/trac/cactus/browser/$MP7FW_REPO_SUBDIR/cactusupgrades/components/mp7_ttc?rev=HEAD\&format=zip
+    unzip mp7_ttc.zip
+    rm -f mp7_ttc.zip
+    popd
+    popd
+fi
